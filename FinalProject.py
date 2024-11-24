0282cee110aa6b4fac54b723d61634289a787b4b
Michael Bergstrom <144189188+mbergstrom37@users.noreply.github.com>
2024-11-23 20:49:10 -0500

    Add files via upload
    Python file we are using with comment pseudocode.

---
+++ FinalProject.py
@@ -0,0 +1,25 @@
+#Receiving sound data
+	#check for .wav
+		#if yes keep going
+		#if no convert to .wav
+	#check for number of channels
+		#if 1 channel keep going
+		#if 2 channels, make two .wav files one for left channel one for right channel
+	#check for metadata
+		#if yes remove metadata
+		#if no keep going
+
+
+#Plotting graphs and analysing data
+	#display waveform graph/graphs
+	#compute RT60 for low medium and high frequency
+	#display RT60 graphs for low medium and high frequency
+	#display difference in RT60 to reduce to 0.5 seconds
+	#display specgram graph
+
+#Making Gui
+	#button to load audio file
+	#button to combine plots into single plot
+	#button for specgram graph
+
+				#structure subject to change depending on needs
