41b0fd563a1f5b6a44648bf652a439d1ae270b65
Michael Bergstrom <mbergstrom5060@floridapoly.edu>
2024-12-02 15:44:18 -0500

    Waveform Plot functions

--- FinalProject.py
+++ FinalProject.py
@@ -1,3 +1,10 @@
+from scipy.io import wavfile
+import scipy.io
+import wave
+import matplotlib.pyplot as plt
+import numpy as np
+import tkinter as tk
+import matplotlib.figure as figure
 #Receiving sound data
 	#check for .wav
 		#if yes keep going
@@ -11,11 +18,28 @@
 
 
 #Plotting graphs and analysing data
-	#display waveform graph/graphs
-	#compute RT60 for low medium and high frequency
-	#display RT60 graphs for low medium and high frequency
-	#display difference in RT60 to reduce to 0.5 seconds
-	#display specgram graph
+#display waveform graph
+def display_wave_1channel(data, length) :
+	time = np.linspace(0., length, data.shape[0])
+	plt.plot(time, data, label="Single channel")
+	plt.xlabel("Time [s]")
+	plt.ylabel("Amplitude")
+	plt.show()
+def display_wave_2channel(data, length) :
+	time = np.linspace(0., length, data.shape[0])
+	plt.plot(time, data[:, 0], label="Left channel")
+	plt.plot(time, data[:, 1], label="Right channel")
+	plt.legend()
+	plt.xlabel("Time [s]")
+	plt.ylabel("Amplitude")
+	plt.show()
+
+#compute RT60 for low medium and high frequency
+
+
+#display RT60 graphs for low medium and high frequency
+#display difference in RT60 to reduce to 0.5 seconds
+#display specgram graph
 
 #Making Gui
 	#button to load audio file
