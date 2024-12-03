52a19293ab6c79196b1180f44cb4effc7e63621f
matthew powrie <matthew.powrie20@gmail.com>
2024-12-03 10:38:35 -0500

    L23 program

--- FinalProject.py
+++ FinalProject.py
@@ -1,5 +1,19 @@
+# ????
 import scipy.io as sc
 import matplotlib.pyplot as plt
+
+# GUI Imports
+import argparse
+import base64
+import json
+from pathlib import Path
+from bs4 import BeautifulSoup
+import requests
+import tkinter as tk
+from tkinter import ttk, filedialog as fd, messagebox
+
+from matplotlib.backend_bases import button_press_handler
+
 #Receiving sound data
 	#check for .wav
 		#if yes keep going
@@ -19,9 +33,31 @@ import matplotlib.pyplot as plt
 	#display difference in RT60 to reduce to 0.5 seconds
 	#display specgram graph
 
-#Making Gui
-	#button to load audio file
-	#button to combine plots into single plot
-	#button for specgram graph
+# Making Gui
 
-				#structure subject to change depending on needs
+root = tk.Tk()
+root.title("SPIDAM Sound Analysis")
+root.geometry("1500x600")
+
+# frame
+my_frame = ttk.LabelFrame(root, text="File Options", padding="5 5 5 5")
+my_frame.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+
+#
+
+# button to load audio file
+file_button = ttk.Button(my_frame, text="Open File")
+file_button.grid(row=0, column=0, padx=5, pady=5)
+
+# button for specgram graph
+analyze_button = ttk.Button(my_frame, text="Analyze")
+analyze_button.grid(row=0, column=1, padx=5, pady=5)
+# button
+
+root.mainloop()
+
+
+    # button to combine plots into single plot
+
+
+            # structure subject to change depending on needs
